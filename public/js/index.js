eed81ddfd09af2232280b321da3165be177e1de8
Rafae Waqar Qazi <rafaewaqar@gmail.com>
2018-08-03 16:45:00 +0500

    greet new User

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 socket.on('connect',function(){
     console.log('Connected to server');
 
-    socket.emit('createMessage',{
-        from:'rafaewaqar@gmail.com',
-        text:'Hi'
-    });
-
     socket.on('newMessage',function(message){
         console.log('newMessage',message);
         
--- server/server.js
+++ server/server.js
@@ -15,14 +15,30 @@ io.on('connection',(socket)=>{
     console.log('New User Connected!');
     
     socket.emit('newMessage',{
-        from:'Rafae',
-        text:'See Ya',
-        createdAt:123445
+        from:'Admin',
+        text:'Welcome to the Chat App',
+        createdAt:new Date().getTime()
     });
 
+
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New User Joined',
+        createdAt:new Date().getTime()
+    });
     socket.on('createMessage',(message)=>{
         console.log('Create Message',message);
-        
+        io.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createdAt: new Date().getTime()
+        });
+
+        socket.broadcast.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect',()=>{
