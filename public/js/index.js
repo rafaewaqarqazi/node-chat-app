501214f9eee11cf8456c13a22a4a56c81aa174c4
Rafae Waqar Qazi <rafaewaqar@gmail.com>
2018-08-05 18:18:26 +0500

    Form Created and JQuery Added

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,30 @@ var socket = io();
 socket.on('connect',function(){
     console.log('Connected to server');
 
-    socket.on('newMessage',function(message){
-        console.log('newMessage',message);
-        
-    });
 });
 
 socket.on('disconnect',function(){
     console.log('Disconnected!');
 });
 
+socket.on('newMessage',function(message){
+    console.log('newMessage',message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+
+
+
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+        from:'User',
+        text:jQuery('[name=message]').val()
+    },function(){
+
+    });
+});
--- server/server.js
+++ server/server.js
@@ -18,11 +18,12 @@ io.on('connection',(socket)=>{
 
 
     socket.broadcast.emit('newMessage',generateMessage('Admin','New User Joined'));
-    socket.on('createMessage',(message)=>{
+    socket.on('createMessage',(message,callback)=>{
         console.log('Create Message',message);
         io.emit('newMessage',generateMessage(message.from,message.text));
-
-        socket.broadcast.emit('newMessage',generateMessage(message.from,message.text));
+        
+        //socket.broadcast.emit('newMessage',generateMessage(message.from,message.text));
+        callback('This is from server');
     });
 
     socket.on('disconnect',()=>{
